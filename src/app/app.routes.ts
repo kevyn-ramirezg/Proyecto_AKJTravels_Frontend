6a971f6b0b6c90ffe32b20d9d55ecf071cc4dcbf
JuanchoMB <juand.mejiab@uqvirtual.edu.co>
2025-11-11 23:27:23 -0500

    commit contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,27 +3,31 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { CreatePlace } from './pages/create-place/create-place';
-import {MyPlaces} from './pages/my-places/my-places';
-import {DetailPlace} from './pages/detail-place/detail-place';
-import {HostDashboard,} from './pages/host-dashboard/host-dashboard';
-import {loginGuard} from './guards/login-guard';
-import {roleGuard} from './guards/role-guard';
-import {hostGuard} from './guards/host.guard';
-
-import {authGuard} from './guards/auth-guard';
-
+import { MyPlaces } from './pages/my-places/my-places';
+import { DetailPlace } from './pages/detail-place/detail-place';
+import { HostDashboard } from './pages/host-dashboard/host-dashboard';
+import { loginGuard } from './guards/login-guard';
+import { roleGuard } from './guards/role-guard';
+import { hostGuard } from './guards/host.guard';
+import { authGuard } from './guards/auth-guard';
+import { ForgotPassword } from './pages/forgot-password/forgot-password';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login, canActivate: [loginGuard] },
-  { path: 'register', component: Register, canActivate: [loginGuard]  },
-  { path: "my-places", component: MyPlaces, canActivate: [authGuard,roleGuard], data: { expectedRole:['HOST', 'ROLE_HOST'] } },
-  { path: 'create-place', component: CreatePlace, canActivate: [authGuard, roleGuard],
-    data: { expectedRole: ['HOST', 'ROLE_HOST'] }},
+  { path: 'register', component: Register, canActivate: [loginGuard] },
+  { path: 'my-places', component: MyPlaces, canActivate: [authGuard, roleGuard], data: { expectedRole: ['HOST', 'ROLE_HOST'] } },
+  { path: 'create-place', component: CreatePlace, canActivate: [authGuard, roleGuard], data: { expectedRole: ['HOST', 'ROLE_HOST'] } },
   { path: 'place/:id', component: DetailPlace },
-  { path: 'host-dashboard',component:HostDashboard, canActivate: [authGuard, roleGuard],
-    data: { expectedRole: ['HOST', 'ROLE_HOST'] }},
-  { path: "**", pathMatch: "full", redirectTo: "" },
+  { path: 'host-dashboard', component: HostDashboard, canActivate: [authGuard, roleGuard], data: { expectedRole: ['HOST', 'ROLE_HOST'] } },
 
+  // ⬇️ NUEVO: grupo /auth con la ruta forgot-password (SIN guards)
+  {
+    path: 'auth',
+    children: [
+      { path: 'forgot-password', component: ForgotPassword, canActivate: [loginGuard] },
+    ]
+  },
 
+  { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
