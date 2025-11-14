b6e2416f75600aed74b4fb6a02e16d9bb85c032d
JuanchoMB <juand.mejiab@uqvirtual.edu.co>
2025-11-13 23:14:28 -0500

    Search-results

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,30 +10,33 @@ import { loginGuard } from './guards/login-guard';
 import { roleGuard } from './guards/role-guard';
 import { authGuard } from './guards/auth-guard';
 import { ForgotPassword } from './pages/forgot-password/forgot-password';
-
-// ⬇️ Nuevo: componente de edición de perfil (standalone)
 import { EditProfile } from './pages/edit-profile/edit-profile';
-import {EditPlace} from './pages/edit-place/edit-place';
+import { EditPlace } from './pages/edit-place/edit-place';
+import { SearchResultsComponent } from './pages/search-results/search-results';
 
 export const routes: Routes = [
   { path: '', component: Home },
 
-  // Auth públicas (con guard que evita entrar si ya estás logueado)
+  // Auth públicas
   { path: 'login', component: Login, canActivate: [loginGuard] },
   { path: 'register', component: Register, canActivate: [loginGuard] },
 
-  // Perfil del usuario (PROTEGIDO)
+  // Perfil
   { path: 'mi-perfil', component: EditProfile, canActivate: [authGuard] },
 
-  // Zonas de HOST (PROTEGIDAS por rol)
+  // Host
   { path: 'my-places', component: MyPlaces, canActivate: [authGuard, roleGuard], data: { expectedRole: ['HOST', 'ROLE_HOST'] } },
   { path: 'create-place', component: CreatePlace, canActivate: [authGuard, roleGuard], data: { expectedRole: ['HOST', 'ROLE_HOST'] } },
   { path: 'host-dashboard', component: HostDashboard, canActivate: [authGuard, roleGuard], data: { expectedRole: ['HOST', 'ROLE_HOST'] } },
-  {path: 'edit-place/:id', component: EditPlace, canActivate: [authGuard]},
+  { path: 'edit-place/:id', component: EditPlace, canActivate: [authGuard] },
+
   // Público
   { path: 'place/:id', component: DetailPlace },
 
-  // Grupo /auth (público)
+  // Resultados de búsqueda
+  { path: 'search', component: SearchResultsComponent },
+
+  // Grupo /auth
   {
     path: 'auth',
     children: [
@@ -41,5 +44,6 @@ export const routes: Routes = [
     ]
   },
 
+  // Wildcard SIEMPRE al final
   { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
